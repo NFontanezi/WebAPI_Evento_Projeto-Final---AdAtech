a08ca52101c4e001d8973e40b3bd85327b7c2c9f
Natalia Fontanezi <natalia.fontanezi@gmail.com>
2022-09-14 14:31:21 -0300

    implementing Exception filter

---
+++ Projeto WebAPI-Evento/Filters/GeneralExceptionFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Projeto_WebAPI_Evento.Filters
+{
+    public class GeneralExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var problem = new ProblemDetails
+            {
+                Status = 500,
+                Title = "Erro Inesperado",
+                Detail = "Ocorreu um erro inesperado na solicitação",
+                Type = context.Exception.GetType().Name
+            };
+
+            Console.WriteLine($"Tipo da exceção {context.Exception.GetType().Name}, mensagem {context.Exception.Message}, stack trace {context.Exception.StackTrace}");
+
+            switch (context.Exception)
+            { 
+
+               case ArgumentNullException:
+                    context.Result = new ObjectResult(problem)
+                    {
+                        StatusCode = StatusCodes.Status501NotImplemented
+                    };
+                    break;
+
+                case FormatException:
+                    context.Result = new ObjectResult(problem)
+                    {
+                        StatusCode = StatusCodes.Status501NotImplemented // culpa de quem?
+                    };
+                    break;
+                default:
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Result = new ObjectResult(problem);
+                    break;
+
+            }
+    }
+}
